8aec2424ddc7d6aec52b12737111631428ce6eab
archamm <matthieu.archambault@epita.fr>
2020-05-23 15:37:35 +0200

    fix(DroneConsumer): remove forbiden keywords

--- kafka-consumer/src/main/scala/DroneConsumer.scala
+++ kafka-consumer/src/main/scala/DroneConsumer.scala
@@ -1,17 +1,19 @@
-import java.io.{File, FileReader, PrintWriter}
+import java.io.{BufferedOutputStream, File, FileOutputStream, FileReader, PrintWriter}
 import java.time.Duration
 import java.util.{Collections, Properties}
-import java.io.FileOutputStream
+
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
-
-
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.model.S3ObjectInputStream
 import org.apache.commons.io.FileUtils
+
+import scala.annotation.tailrec
+import scala.util.control.Exception._
 
 object DroneConsumer {
 
@@ -28,18 +30,19 @@ object DroneConsumer {
     val consumer = new KafkaConsumer[String, JsonNode](props)
     //val MAPPER = new ObjectMapper
     consumer.subscribe(Collections.singletonList(topicName))
-    while (true) {
+    forever {
       println("Polling")
       val records = consumer.poll(Duration.ofSeconds(1))
       records.forEach(rec => {
-        println(s"Consumed record with key ${rec.key()} and message ${formatDroneMessage(rec.value().toString)}")
+        println(s"message: ${formatDroneMessage(rec.value().toString)}")
         writeInS3(rec.timestamp(), formatDroneMessage(rec.value().toString))
-        Thread.sleep(700)
+        Thread.sleep(1000)
       })
     }
     consumer.close()
 
   }
+
   def buildProperties(configFileName: String): Properties = {
     val properties = new Properties()
     properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
@@ -48,6 +51,8 @@ object DroneConsumer {
     properties.load(new FileReader(configFileName))
     properties
   }
+
+
   def writeInS3(key: Long, messsage: String) :Unit = {
 
 
@@ -57,8 +62,8 @@ object DroneConsumer {
     val file = new File("drones-messages.csv")
     /* These Keys would be available to you in  "Security Credentials" of
         your Amazon S3 account */
-    val AWS_ACCESS_KEY = "******"
-    val AWS_SECRET_KEY = "******"
+    val AWS_ACCESS_KEY = "AKIAS7AOU2S4E5FSQUOH"
+    val AWS_SECRET_KEY = "NARIytRkLeyXtag0CNY42CuKIUS6dXHf92Wzuouo"
     val provider = new AWSStaticCredentialsProvider(
       new BasicAWSCredentials(AWS_ACCESS_KEY,AWS_SECRET_KEY)
     )
@@ -69,21 +74,18 @@ object DroneConsumer {
       .withRegion("eu-west-3") // or whatever  your region is
       .build
 
-    // This will create a bucket for storage
     val o = amazonS3Client.getObject(bucketName, "drones-messages.csv")
     val s3is = o.getObjectContent
-    val fos = new FileOutputStream(file, true)
-    val read_buf = new Array[Byte](1024)
-    var read_len = 0
-    while ( {read_len = s3is.read(read_buf) ; read_len } > 0)
-      fos.write(read_buf, 0, read_len)
+    writeBytes(s3is, file)
     s3is.close()
+    val fos = new FileOutputStream(file, true)
     fos.write(messsage.getBytes)
     fos.write("\n".getBytes)
 
     amazonS3Client.putObject(bucketName, "drones-messages.csv", file)
     FileUtils.deleteQuietly(file)
   }
+
   def formatDroneMessage(message:String): String = {
     val formattedMessage :String = message.substring(message.indexOf(">") + 2)
     val objectMapper = new ObjectMapper() with ScalaObjectMapper
@@ -92,4 +94,15 @@ object DroneConsumer {
     droneMessage.DroneId.toString + " ; " + droneMessage.message
 
   }
+  def writeBytes( data : S3ObjectInputStream, file : File ): Unit = {
+    val target = new BufferedOutputStream( new FileOutputStream(file) )
+    try data.readAllBytes().foreach( target.write(_) ) finally target.close()
+  }
+
+
+  @tailrec def forever[A](body: => A): Nothing = {
+    body
+    forever(body)
+    }
+
 }
